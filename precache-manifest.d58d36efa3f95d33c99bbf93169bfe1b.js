a71c3099bbe88cb41a85552a4fded30d9b9e932b
CORP\sarangku <sarang.c.kulkarni@capgemini.com>
2022-10-29 14:58:03 +1100

    Updates

---
+++ precache-manifest.d58d36efa3f95d33c99bbf93169bfe1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a625eaf042d6c888de328eb77879b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "215e8fe4c4244b513b17",
+    "url": "/static/css/main.54f1c2fc.chunk.css"
+  },
+  {
+    "revision": "bf0b48b3b3d4220c4ca1",
+    "url": "/static/js/2.7873710c.chunk.js"
+  },
+  {
+    "revision": "a35aa9733af881a70cd0df68fb1281e7",
+    "url": "/static/js/2.7873710c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215e8fe4c4244b513b17",
+    "url": "/static/js/main.daea5b1d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.daea5b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951fe510ccb58f419fda",
+    "url": "/static/js/runtime-main.5b7b5dc1.js"
+  },
+  {
+    "revision": "17260a1ff66043873c69b96542812f7f",
+    "url": "/static/media/foxtel.17260a1f.svg"
+  },
+  {
+    "revision": "f227c489ca8dcfded430ecc5a71d600f",
+    "url": "/static/media/icon-close.f227c489.svg"
+  },
+  {
+    "revision": "c507a81b69b4156a009a88f0d0bce903",
+    "url": "/static/media/icon-loading.c507a81b.svg"
+  }
+]);
