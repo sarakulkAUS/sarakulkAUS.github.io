c15c2d6fb3e6766cd9ccbbf4e38e52421ef784b2
CORP\sarangku <sarang.c.kulkarni@capgemini.com>
2022-11-03 00:48:50 +1100

    Updated env to connect to AEM server

---
+++ build/precache-manifest.0d7141b452cef817a90e2a5b52095b26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733eee654835873a6632d0da6ecc829e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10edb4afb1995c359d45",
+    "url": "/static/css/main.54f1c2fc.chunk.css"
+  },
+  {
+    "revision": "bf0b48b3b3d4220c4ca1",
+    "url": "/static/js/2.7873710c.chunk.js"
+  },
+  {
+    "revision": "a35aa9733af881a70cd0df68fb1281e7",
+    "url": "/static/js/2.7873710c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10edb4afb1995c359d45",
+    "url": "/static/js/main.ff3b9e70.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ff3b9e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951fe510ccb58f419fda",
+    "url": "/static/js/runtime-main.5b7b5dc1.js"
+  },
+  {
+    "revision": "17260a1ff66043873c69b96542812f7f",
+    "url": "/static/media/foxtel.17260a1f.svg"
+  },
+  {
+    "revision": "f227c489ca8dcfded430ecc5a71d600f",
+    "url": "/static/media/icon-close.f227c489.svg"
+  },
+  {
+    "revision": "c507a81b69b4156a009a88f0d0bce903",
+    "url": "/static/media/icon-loading.c507a81b.svg"
+  }
+]);
